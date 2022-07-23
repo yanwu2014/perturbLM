4ef77166d4a25a950f086602b6be904816592099
Yan <yauwning@gmail.com>
2022-07-23 02:22:54 +0000

    Explicitly filter pert dictionary, align parameter names to the rest of perturbLM

--- R/glmnet.R
+++ R/glmnet.R
@@ -12,7 +12,7 @@ NULL
 #'
 #' @param x Design matrix + covariates matrix
 #' @param y Expression response
-#' @param pert_dict Perturbation dictionary
+#' @param groups.list Perturbation dictionary
 #' @param metric Metric to use for evaluation (pearson, spearman)
 #' @param alpha Alpha value
 #' @param family GLM family to use for elasticnet (default: mgaussian)
@@ -24,13 +24,12 @@ NULL
 #'
 cross_validate_lambda <- function(x,
                                   y,
-                                  pert_dict,
+                                  groups.list,
                                   alpha,
                                   metric,
                                   family,
                                   lambda,
                                   folds) {
-
   fold_err <- lapply(folds, function(split.ix) {
     train.ix <- split.ix == "train"
     test.ix <- split.ix == "test"
@@ -41,8 +40,8 @@ cross_validate_lambda <- function(x,
     y.test[is.na(y.test)] <- 0
     rownames(y.test) <- rownames(y)[test.ix]
 
-    pert_dict_sub <- lapply(pert_dict, function(cells) intersect(cells, rownames(y.test)))
-    y.test.avg <- lapply(pert_dict_sub, function(ix) colMeans(y.test[ix,]))
+    groups.list.sub <- lapply(groups.list, function(cells) intersect(cells, rownames(y.test)))
+    y.test.avg <- lapply(groups.list.sub, function(ix) colMeans(y.test[ix,]))
     y.test.avg <- do.call(rbind, y.test.avg)
 
     fit_train <- glmnet(x[train.ix,], y[train.ix, colnames(y.test)], family = family, alpha = alpha, lambda = lambda)
@@ -53,7 +52,7 @@ cross_validate_lambda <- function(x,
       y.pred[is.na(y.pred)] <- 0
       rownames(y.pred) <- rownames(y)[test.ix]
 
-      y.pred.avg <- lapply(pert_dict_sub, function(ix) colMeans(y.pred[ix,]))
+      y.pred.avg <- lapply(groups.list.sub, function(ix) colMeans(y.pred[ix,]))
       y.pred.avg <- do.call(rbind, y.pred.avg)
       mean(sapply(1:nrow(y.pred.avg), function(i) cor(y.pred.avg[i,], y.test.avg[i,], method = metric)))
     })
@@ -86,7 +85,7 @@ cross_validate_lambda <- function(x,
 #'
 RunCrossValidation <- function(x,
                                y,
-                               pert_dict,
+                               groups.list,
                                metric = "pearson",
                                alpha.seq = seq(0, 1, by = 0.2),
                                plot = T,
@@ -104,20 +103,21 @@ RunCrossValidation <- function(x,
   }
 
   ## Get folds
-  folds <- SplitFoldsByGroup(pert_dict, nfolds, seed = seed)
+  groups.list <- lapply(groups.list, function(cells) intersect(cells, rownames(x)))
+  folds <- SplitFoldsByGroup(groups.list, nfolds, seed = seed)
 
-  cl <- snow::makeCluster(n.cores, type = "SOCK")
-  snow::clusterExport(cl, c("x", "y", "pert_dict", "metric", "family", "nlambda", "lambda.min.ratio", "folds"),
-                      envir = environment())
-  invisible(snow::parLapply(cl, 1:n.cores, function(i) library(glmnet)))
-  cv_list <- parLapply(cl, alpha.seq, function(a) {
+  # cl <- snow::makeCluster(n.cores, type = "SOCK")
+  # snow::clusterExport(cl, c("x", "y", "groups.list", "metric", "family", "nlambda", "lambda.min.ratio", "folds"),
+  #                     envir = environment())
+  # invisible(snow::parLapply(cl, 1:n.cores, function(i) library(glmnet)))
+  cv_list <- lapply(alpha.seq, function(a) {
     ## Get lambda sequence
     fit_full <- glmnet(x, y, family = family, alpha = a, nlambda = nlambda, lambda.min.ratio = lambda.min.ratio)
     lambda <- fit_full$lambda
 
     fold_err <- cross_validate_lambda(x = x,
                                       y = y,
-                                      pert_dict = pert_dict,
+                                      groups.list = groups.list,
                                       alpha = a,
                                       metric = metric,
                                       family = family,
@@ -126,7 +126,7 @@ RunCrossValidation <- function(x,
 
     data.frame(lambda = lambda, log.lambda = log(lambda), alpha = a, error = colMeans(fold_err))
   })
-  on.exit(snow::stopCluster(cl))
+  # on.exit(snow::stopCluster(cl))
 
   cv_df <- do.call(rbind, cv_list)
   cv_df$alpha_fac <- as.factor(cv_df$alpha)
